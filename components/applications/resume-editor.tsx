8f3095f62e5fbb9e0bb6e0ef666e566d2f5747c5
NajibOladosu <najiboladosu@gmail.com>
2026-01-05 22:15:06 +0100

    fix: Update react-pdf CSS import paths by removing 'esm/' from the directory structure.

--- components/applications/resume-editor.tsx
+++ components/applications/resume-editor.tsx
@@ -7,8 +7,8 @@ import { ArrowLeft, Download, FileText, Loader2, Save, Undo, ZoomIn, ZoomOut } f
 import { ResumeFeedback, type ResumeAnalysisResult } from "./resume-feedback"
 import { Document, Page, pdfjs } from "react-pdf"
 import { useResizeObserver } from "usehooks-ts"
-import "react-pdf/dist/esm/Page/AnnotationLayer.css"
-import "react-pdf/dist/esm/Page/TextLayer.css"
+import "react-pdf/dist/Page/AnnotationLayer.css"
+import "react-pdf/dist/Page/TextLayer.css"
 
 // Configure PDF.js worker
 pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
